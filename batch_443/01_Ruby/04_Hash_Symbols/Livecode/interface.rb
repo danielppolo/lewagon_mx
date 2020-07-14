51fe7e2dbae49c9a6ce490795902e9e56422f416
Daniel Polo <od.polo@gmail.com>
2020-07-14 10:51:09 -0500

    Day 7

--- batch_443/01_Ruby/04_Hash_Symbols/Livecode/interface.rb
+++ batch_443/01_Ruby/04_Hash_Symbols/Livecode/interface.rb
@@ -1,1 +1,26 @@
-Don't copy 💩
+require_relative 'frequencies'
+
+# Enter the file
+file_path = 'dummy.txt'
+
+# Read file
+content = File.read(file_path)
+
+# Pass the content of the file to method
+count = frequencies(content)
+
+# Convert a hash into array
+sorted_count = count.sort_by do |key, value|
+  value
+end.reverse
+
+# Iterate through it and sort it
+sorted_count.each do |key, value|
+  puts "#{value} - #{key}"
+end
+
+# Print the results
+# GOAL 👇
+# 20 - Dog
+# 18 - the
+# 15 - Cat
