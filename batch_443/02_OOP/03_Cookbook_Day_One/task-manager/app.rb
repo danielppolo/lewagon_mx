b0c08e560c7fe9bca6bffb5bca4fab3c730b4b52
Caio Andrade <caioertai@gmail.com>
2020-07-17 11:56:50 -0500

    Remove task adding from app

--- batch_443/02_OOP/03_Cookbook_Day_One/task-manager/app.rb
+++ batch_443/02_OOP/03_Cookbook_Day_One/task-manager/app.rb
@@ -6,8 +6,4 @@ repo = TaskRepository.new
 controller = TasksController.new(repo)
 router = Router.new(controller)
 
-# Adding tasks on init
-walk_the_dog = Task.new("Walk the dog")
-repo.add(walk_the_dog)
-
 router.run
