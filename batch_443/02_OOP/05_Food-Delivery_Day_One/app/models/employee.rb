aa01e4aa14506aa687cd3287c7208dfbb0dac430
Ignacio Alonso <ignacio.alley@gmail.com>
2020-07-22 10:12:37 -0500

    Add logic for sessions

---
+++ batch_443/02_OOP/05_Food-Delivery_Day_One/app/models/employee.rb
@@ -0,0 +1,19 @@
+class Employee
+  attr_accessor :id
+  attr_reader :username, :password, :role
+
+  def initialize(attrs = {})
+    @id       = attrs[:id]
+    @username = attrs[:username]
+    @password = attrs[:password]
+    @role     = attrs[:role]
+  end
+
+  def manager?
+    @role == 'manager'
+  end
+
+  def delivery_guy?
+    @role == 'delivery_guy'
+  end
+end
---
+++ batch_443/02_OOP/05_Food-Delivery_Day_One/app/repositories/employee_repository.rb
@@ -0,0 +1,37 @@
+require 'csv'
+require_relative '../models/employee'
+
+class EmployeeRepository
+  def initialize(csv_file)
+    @csv_file  = csv_file
+    @employees = []
+
+    load_csv if File.exist?(@csv_file)
+  end
+
+  def all_delivery_guys
+    @employees.select { |employee| employee.delivery_guy? }
+  end
+
+  # Needed to select employee based on index
+  def find(id)
+    @employees.find { |employee| employee.id == id }
+  end
+
+  # Needed to search for the employee based on username on login
+  def find_by_username(username)
+    @employees.find { |employee| employee.username == username }
+  end
+
+  private
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+
+    CSV.foreach(@csv_file, csv_options) do |row|
+      row[:id]       = row[:id].to_i # Convert column to Integer
+
+      @employees << Employee.new(row)
+    end
+  end
+end
